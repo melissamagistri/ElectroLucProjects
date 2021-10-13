4e36d908adb71fe8877bfb99f8ffac7924d08140
Melissa Magistri <melissamagistri@Air-di-Melissa.fritz.box>
2021-10-13 18:33:51 +0200

    Add eployee product controller

---
+++ src/application/AvaliabilityProductController.java
@@ -0,0 +1,55 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AvaliabilityProductController {
+
+    @FXML
+    private Button GoBackButton;
+
+    @FXML
+    private Button ListButton;
+
+    @FXML
+    private TableColumn<?, ?> NameTableColuomn;
+
+    @FXML
+    private TableColumn<?, ?> NumberTableColoumn;
+
+    @FXML
+    private TableColumn<?, ?> PriceTableColuomn;
+
+    @FXML
+    private TableView<?> ProductTableView;
+
+    @FXML
+    private TextField SearchBar;
+
+    @FXML
+    private Button SearchProductButton;
+
+    @FXML
+    private TableColumn<?, ?> StateTableColumn;
+
+    @FXML
+    void OnCLickGoBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickSearchButton(ActionEvent event) {
+
+    }
+
+}
+
