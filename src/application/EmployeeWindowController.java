164f27e6ffbf9ba729c05cb3919f78330f95f573
Melissa Magistri <melissamagistri@Air-di-Melissa.fritz.box>
2021-10-13 18:00:19 +0200

    Add the employee window

---
+++ src/application/EmployeeWindowController.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class EmployeeWindowController {
+
+    @FXML
+    private Button CheckAvailabilityButton;
+
+    @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private Button ValidWarrantyButton;
+
+    @FXML
+    void OnClickCheckAvailability(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickValidWarranty(ActionEvent event) {
+
+    }
+
+}
+
