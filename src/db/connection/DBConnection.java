decb9203d1075d6a396f5d808b302df1fc49e155
Federico <federico@KPLW0X65ee4b75.fritz.box>
2021-10-27 11:08:48 +0200

    fix the connection to the db

--- src/db/connection/DBConnection.java
+++ src/db/connection/DBConnection.java
@@ -5,13 +5,13 @@ import java.sql.*;
 public class DBConnection {
     
     //nome del database
-    private final String DB_NAME = "pippodb";
+    private final String DB_NAME = "negozio elettronica";
 
     //MySQL locale
     final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	final String DB_URI = "jdbc:mysql://127.0.0.1:3306/" + DB_NAME;
 	final String DB_USER_NAME = "root";
-	final String DB_PASSWORD = "luccarino";
+	final String DB_PASSWORD = "LucCarino2000@";
 
 	public Connection getMySQLConnection()  {
 		
