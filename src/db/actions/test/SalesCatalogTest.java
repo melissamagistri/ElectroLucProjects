32468b49b944bb72f5fc71c561f68ca34d4606d4
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-10-27 14:13:03 +0200

    created the action for insert or remove a model from sales catalog, its test and modified .classpath

---
+++ src/db/actions/test/SalesCatalogTest.java
@@ -0,0 +1,11 @@
+package db.actions.test;
+
+import db.actions.SalesCatalog;
+
+public class SalesCatalogTest {
+
+	public static void main(String[] args) {
+		SalesCatalog.remove(1);
+		SalesCatalog.remove(2);
+	}
+}
