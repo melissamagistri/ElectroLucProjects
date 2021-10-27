ac0b20bd6d1b7ba0864be0ef862e62c76c63f6ee
Federico <federico@KPLW0X65ee4b75.fritz.box>
2021-10-27 16:42:22 +0200

    add the connection to the db in the login of both Customer and Employee

--- src/application/Customer/LoginController.java
+++ src/application/Customer/LoginController.java
@@ -1,9 +1,15 @@
 package application.Customer;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import db.connection.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginController {
 
@@ -14,8 +20,27 @@ public class LoginController {
     private TextField tx_email;
 
     @FXML
-    void OnClickLogin(ActionEvent event) throws IOException {
-    	CustomerMain.changeWindow("ClientWindow.fxml");
+    void OnClickLogin(ActionEvent event) throws IOException, SQLException {
+    	if(this.tx_email.getText().equals("")) {
+    		Alert alert = new Alert(AlertType.ERROR,"You need to insert an email");
+    		alert.show();
+    	} else if(this.tx_Password.getText().equals("")){
+    		Alert alert = new Alert(AlertType.ERROR, "You need to insert a password");
+    		alert.show();
+    	} else {
+    		DBConnection connection = new DBConnection();
+    		String sql = "select * from `negozio elettronica`.customers_accounts where Email = '"
+    		+ this.tx_email.getText() + "'" + " and Password ='" + this.tx_Password.getText() + "'";
+    		Statement statement = connection.getMySQLConnection().createStatement();
+    		ResultSet resultSet = statement.executeQuery(sql);
+    		
+    		if(!resultSet.isBeforeFirst()) {
+    			Alert alert = new Alert(AlertType.ERROR, "Your credential are wrong");
+        		alert.show();
+    		} else {
+    			CustomerMain.changeWindow("ClientWindow.fxml");
+    		}	
+    	}
     }
 
     @FXML
--- src/application/Employee/LoginController.java
+++ src/application/Employee/LoginController.java
@@ -1,9 +1,16 @@
 package application.Employee;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import application.Customer.CustomerMain;
+import db.connection.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginController {
 
@@ -14,8 +21,27 @@ public class LoginController {
     private TextField tx_email;
 
     @FXML
-    void OnClickLogin(ActionEvent event) throws IOException {
-    	EmployeeMain.changeWindow("EmployeeWindow.fxml");
+    void OnClickLogin(ActionEvent event) throws IOException, SQLException {
+    	if(this.tx_email.getText().equals("")) {
+    		Alert alert = new Alert(AlertType.ERROR,"You need to insert your personal code");
+    		alert.show();
+    	} else if(this.tx_Password.getText().equals("")){
+    		Alert alert = new Alert(AlertType.ERROR, "You need to insert your password");
+    		alert.show();
+    	} else {
+    		DBConnection connection = new DBConnection();
+    		String sql = "select * from `negozio elettronica`.employees_account where EmployeeId = '"
+    		+ this.tx_email.getText() + "'" + " and Passowrd ='" + this.tx_Password.getText() + "'";
+    		Statement statement = connection.getMySQLConnection().createStatement();
+    		ResultSet resultSet = statement.executeQuery(sql);
+    		
+    		if(!resultSet.isBeforeFirst()) {
+    			Alert alert = new Alert(AlertType.ERROR, "Your credential are wrong");
+        		alert.show();
+    		} else {
+    			EmployeeMain.changeWindow("EmployeeWindow.fxml");
+    		}	
+    	}
     }
 
     @FXML
