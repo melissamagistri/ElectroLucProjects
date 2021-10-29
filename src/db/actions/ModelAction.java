bd4f72299b5ee739c9a5b9cbd6811eae9b625ec6
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-10-29 09:34:33 +0200

    added the action to search models that are running out, and added cost tables of actions

--- src/db/actions/ModelAction.java
+++ src/db/actions/ModelAction.java
@@ -42,4 +42,32 @@ public class ModelAction {
 		return res;
 	}
 
+	public static List<Model> searchRunningOutModels(final int minQuantity) throws SQLException{
+		DBConnection conn = new DBConnection();
+
+		List<Model> res = new ArrayList<>();
+		String query = "SELECT * FROM models WHERE UnitInStock <= " +minQuantity +" ORDER BY UnitInStock;";
+		PreparedStatement preparedStmt = conn.getMySQLConnection().prepareStatement(query);
+
+		// execute the query, and get a java resultset
+		ResultSet rs = preparedStmt.executeQuery(query);
+
+		Optional<Blob> image = Optional.empty();
+		Optional<Integer> discount = Optional.empty();
+		Boolean sales;
+
+		// iterate through the java resultset
+		while (rs.next()) {
+			image = Optional.ofNullable(rs.getBlob("ModelImage"));
+		    discount = Optional.ofNullable(rs.getInt("Discount"));
+		    sales = (rs.getInt("SalesCatalogMembership")==1) ? true : false;
+
+		    res.add(new Model(rs.getInt("ModelID"), rs.getString("ModelName"), rs.getString("Brand"),
+		    		rs.getString("Description"), image, rs.getBigDecimal("UnitPrice"),
+		    		discount, rs.getInt("UnitInStock"), rs.getInt("MaxQuantytyPerOrder"), 
+		    		rs.getString("Category"), rs.getString("Shelf"), rs.getString("Lane"), 
+		    		rs.getString("Compartment"), sales));
+		}
+		return res;
+	}
 }
