191ccfaab47ad7a21ae31c5c8e5ccb13fb9a4fc3
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-10-29 10:32:17 +0200

    fixed bug

--- src/model/Receipt.java
+++ src/model/Receipt.java
@@ -1,13 +1,11 @@
 package model;
 
-import java.util.Date;
-
 public class Receipt {
 
 	private final int receiptID;
 	private final int orderID;
 
-	public Receipt(final int orderID, final Date dateIssue) {
+	public Receipt(final int orderID, final int receiptID) {
 		this.receiptID = receiptID;
 		this.orderID = orderID;
 	}
