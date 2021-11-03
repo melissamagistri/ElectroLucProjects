71e05401fb31f1edf1620da897207abdf97b2230
Federico <federico@KPLW0X65ee4b75.fritz.box>
2021-11-03 13:18:26 +0100

    add the select part in mmodifyEmployeeWindow

--- src/application/Holder/ModifyEmployeeController.java
+++ src/application/Holder/ModifyEmployeeController.java
@@ -10,8 +10,10 @@ import java.sql.Statement;
 import db.connection.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class ModifyEmployeeController {
 
@@ -76,8 +78,41 @@ public class ModifyEmployeeController {
     }
 
     @FXML
-    void OnClickSelect(ActionEvent event) {
-
+    void OnClickSelect(ActionEvent event) throws SQLException {
+    	Connection connection;
+    	try {
+			connection = new DBConnection().getMySQLConnection().get();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("there was a problem with the db connection");
+			return;
+		}
+    	if(this.SearchBar.getText().equals("")) {
+    		Alert alert = new Alert(AlertType.ERROR, "You need to insert an id");
+    		alert.show();
+    	} else {
+    		String sql = "select * from `negozio elettronica`.employees where EmployeeID = '"
+    				+this.SearchBar.getText()+ "'";
+    		Statement statement = connection.createStatement();
+    		ResultSet resultSet = statement.executeQuery(sql);
+    		
+    		if(!resultSet.isBeforeFirst()) {
+    			Alert alert = new Alert(AlertType.ERROR, "The id that you insert doesn't match with any of the employee");
+        		alert.show();
+    		} else {
+    			resultSet.next();
+        		this.txName.setText(resultSet.getString("FirstName"));
+        		this.txSurname.setText(resultSet.getString("LastName"));
+        		this.txFiscalCode.setText(resultSet.getString("Fiscalcode"));
+        		this.txIDCode.setText(resultSet.getString("EmployeeID"));
+        		this.txSalary.setText(resultSet.getString("Salary"));
+        		sql = "select * from `negozio elettronica`.contract where EmployeeID = '"
+        				+this.SearchBar.getText()+ "'";
+        		statement = connection.createStatement();
+        		resultSet = statement.executeQuery(sql);
+        		resultSet.next();
+        		this.txContract.setText(resultSet.getString("ContractType"));
+    		}
+    	}
     }
     
     @FXML
