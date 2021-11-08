f70a7e4c28130bb62e74e6aa9da0645c07694dae
Melissa Magistri <melissamagistri@192.168.1.54>
2021-11-08 11:56:35 +0100

    Add update unit

---
+++ src/application/Holder/UpdateUnitController.java
@@ -0,0 +1,16 @@
+package application.Holder;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class UpdateUnitController {
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    void OnClickAdd(ActionEvent event) {
+    	
+    }
+
+}
