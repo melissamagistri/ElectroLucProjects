4bedf7ade034f9e373158526083ef91901adc9cb
Melissa Magistri <melissamagistri@192.168.1.55>
2021-11-08 20:59:59 +0100

    Add the id for the table in sell

--- src/application/Employee/SellWindowController.java
+++ src/application/Employee/SellWindowController.java
@@ -4,8 +4,32 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class SellWindowController {
+	
+	 	@FXML
+	    private TableColumn<?, ?> IDcolumn;
+
+	    @FXML
+	    private TableColumn<?, ?> descriptionColumn;
+
+	    @FXML
+	    private TableColumn<?, ?> nameColumn;
+
+	    @FXML
+	    private TableColumn<?, ?> priceColumn;
+
+	    @FXML
+	    private TableColumn<?, ?> quantityColumn;
+
+	    @FXML
+	    private TextField searchTextField;
+
+	    @FXML
+	    private TableView<?> tableview;
 
     @FXML
     void OnClickGoBack(ActionEvent event) throws IOException {
