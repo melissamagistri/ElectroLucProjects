c8b1646408d51ddef3600d2f7de3adb148027c87
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-11-19 18:12:39 +0100

    implemented new type of search product

--- src/db/actions/ActionsOnProduct.java
+++ src/db/actions/ActionsOnProduct.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import util.Pair;
 import model.Model;
 
 public class ActionsOnProduct {
@@ -46,5 +49,50 @@ public class ActionsOnProduct {
 		    	discount, rs.getInt("UnitInStock"),	rs.getString("Category"), sales));
 		}
 		return Optional.empty();
+	}
+
+	public static List<Model> search(final Connection conn, final List<Pair<String,String>> modelAttr) throws SQLException{
+		List<Model> list = new ArrayList<>();
+		boolean inizialize = false;
+		int i = 0;
+		String query = "";
+
+		for(Pair<String,String> condition : modelAttr){
+			if(!condition.getSecond().isBlank()){
+				if(!inizialize){
+					query = query 
+							+"select * "
+							+ "from (select * from models "
+							+ "where " +condition.getFirst() +"=" +condition.getSecond() +") as a" +i++;
+					inizialize = true;
+				} else{
+					query = query 
+							+" NATURAL JOIN "
+							+ "(select ModelID from models "
+							+ "where " +condition.getFirst() +"=" +condition.getSecond() +") as a" +i++;
+				}
+			}
+		}
+
+		if(!query.isBlank()) {
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+
+			// execute the query, and get a java resultset
+			ResultSet rs = preparedStmt.executeQuery(query);
+
+			Optional<Integer> discount = Optional.empty();
+			Boolean sales;
+
+			// iterate through the java resultset
+			while (rs.next()) {
+				discount = Optional.ofNullable(rs.getInt("Discount"));
+				sales = (rs.getInt("InSale")==1) ? true : false;
+
+				list.add(new Model(rs.getInt("ModelID"), rs.getString("ModelName"), rs.getString("Brand"),
+						rs.getString("Description"), rs.getBigDecimal("UnitSellingPrice"),
+						discount, rs.getInt("UnitInStock"),	rs.getString("Category"), sales));
+			}
+		}
+		return list;
 	}
 }
